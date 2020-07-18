4af96cc61d14fce8ece00e260deb313fd3dc51b8
Otis Song <1056822797@qq.com>
2020-07-18 19:55:24 +1000

    set database structure and connect backend to flutter

--- q_people/lib/home.dart
+++ q_people/lib/home.dart
@@ -19,97 +19,56 @@
 //}
 
 import 'package:flutter/material.dart';
+import 'authenticate.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 //import 'package:flappy_search_bar/flappy_search_bar.dart';
-
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-
-class _HomeState extends State<Home> {
-
-  //Map data = {};
-
-  @override
-  Widget build(BuildContext context) {
-
-    //data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
-    //print(data);
-
-    //set background
-    //String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
-    //Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-
-      body: Container(
-
-        child: Stack(
-          children: <Widget>[
-
-            Container(
-                child: Column(
-                  children: <Widget>[
-                      SizedBox(height:12.0),
-                      Text("Top 5 Hazy Suburbs", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, color: Colors.black)
-                      ),
-                  ]
-                )
+//import 'package:cloud_firestore/cloud_firestore.dart';
+class Home extends StatelessWidget {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document){
+    return ListTile(
+      title:Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+                document['shop'],
+                style: Theme.of(context).textTheme.headline6,
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class TopHazy extends StatelessWidget {
-  String city;
-  TopHazy({this.city});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20.0),),
-      ),
-      height: 50,
-      //color: Colors.amber[600],
-      child:  Center(
-          child: Text(
-              city,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0
-              )
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xffddddff)
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              document['Qnumber'].toString(),
+              style: Theme.of(context).textTheme.headline6,
+            ),
           )
+        ],
       ),
+      onTap: () {
+        document.reference.updateData({
+          'Qnumber': document['Qnumber'] + 1
+        });
+      }
     );
   }
-}
-
-
-class Detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            Text("Detail"),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Band Name'),
       ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection(('bandname')).snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading...');
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context,snapshot.data.documents[index]),
+          );
+        }),
     );
   }
 }
-
